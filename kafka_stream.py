33072447f7f6630ff7dd709fc3b78f6d656c9a04
Sujit Thapa <megasuziet@gmail.com>
2024-04-23 14:54:09 +0200

    Initial commit

---
+++ kafka_stream.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+#from airflow.models.dag import DAG
+#from airflow.operators.python import PythonOperator
+import json
+import uuid
+
+def get_data():
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    print(res)
+    res = res['results'][0]
+
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    #data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+def stream_data():
+    res = get_data()
+    res = format_data(res)
+    print(json.dumps(res, indent= 3))
+
+stream_data()
