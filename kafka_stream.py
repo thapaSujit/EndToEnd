8e912cf62b298c5968682de5052c5b328c7a73a7
Sujit Thapa <megasuziet@gmail.com>
2024-04-24 11:12:47 +0200

    update

--- kafka_stream.py
+++ kafka_stream.py
@@ -3,6 +3,7 @@ from datetime import datetime
 #from airflow.operators.python import PythonOperator
 import json
 import uuid
+from kafka import KafkaProducer
 
 def get_data():
     import requests
@@ -37,6 +38,8 @@ def format_data(res):
 def stream_data():
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent= 3))
+    #print(json.dumps(res, indent= 3))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer.send("users_created", json.dumps(res).encode("utf-8"))
 
 stream_data()
